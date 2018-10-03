f3a8ad99fa2e9e336a93b771eb690e204c0ada37
MichaelCastillo <michael.castillo.p96@gmail.com>
2018-10-03 00:33:01 -0300

    se agrega el componente con los auspiciadores (sponsors)

---
+++ src/components/sponsors/Sponsors.vue
@@ -0,0 +1,49 @@
+<style>
+@import url('./Sponsors.css');
+</style>
+
+<template>
+    <div class="container">
+        <div id ="head-sponsors">
+            <h3 class="title">Auspiciadores</h3>
+            <div class="separator-title"></div>
+        </div>
+        <ul class="flex-container wrap">
+        <li
+        class="flex-item"
+        v-for="(value, index) in auspiciadores"
+        v-bind:key="index">
+
+            <img :src="value.link">
+        </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import firebase from 'firebase';
+export default {
+    name:"Sponsors",
+    data(){
+        return{
+            spinner: true,
+            auspiciadores: {}
+        }
+    },
+    methods:{
+        setData(){
+            const starCountRef = firebase.database().ref('/');
+            starCountRef.once('value', (snapshot) => {
+                this.auspiciadores = snapshot.val().auspiciadores;
+                console.log("los tengo");
+                this.spinner = false;
+            });
+        }
+    },
+    mounted() {
+    this.setData();
+  },
+}
+</script>
+
+
