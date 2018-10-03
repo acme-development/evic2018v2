36c32725f4848a7a8db09fe64725410cef16b412
JavierArredondo <javier.arredondo.c@usach.cl>
2018-10-02 23:39:39 -0300

    footer definitivo

---
+++ src/components/footer/Footer.vue
@@ -0,0 +1,37 @@
+<style src="./Footer.css"></style>
+<template>
+    <footer class="site-footer">
+        <div class="wrapperFull wrapperFull--dust-dark">
+			<div class="wrapper wrapper--thin section">
+				<div class="text-group text-center">
+                    <p class="h5 text-white">EVIC anteriores</p>
+					<ul class="social-list">
+                        <li><a href="http://evic.cl/2004/">2004</a></li>
+                        <li><a href="http://evic.cl/2005/">2005</a></li>
+                        <li><a href="http://evic.cl/2006/">2006</a></li>
+                        <li><a href="http://evic.cl/2007/">2007</a></li>
+                        <li><a href="http://evic.cl/2008/">2008</a></li>
+                        <li><a href="http://evic.cl/2009/">2009</a></li>
+                        <li><a href="http://evic.cl/2010/">2010</a></li>
+                        <li><a href="http://evic.cl/2012/">2012</a></li>
+                        <li><a href="http://evic.cl/2015/">2015</a></li>
+                        <li><a href="http://evic.cl/2016/">2016</a></li>
+                        <li><a href="http://evic.cl/2017/">2017</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div class="wrapperFull wrapperFull--dust-brand">
+			<div class="wrapper wrapper--thin section section--short text-center">
+				<p class="text-white">Universidad de Santiago de Chile</p>
+                <p class="text-white">Avenida Libertador Bernardo O'Higgins nº 3363. Estación Central. Santiago. Chile.</p>
+                <p class="text-white">XIV IEEE Escuela de Verano Latinoamericana en Inteligencia Computacional</p>
+			</div>
+		</div>
+    </footer>
+</template>
+<script>
+export default {
+    
+}
+</script>
