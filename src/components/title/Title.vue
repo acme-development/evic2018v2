a04041b12a5c54f329ea15d4b3f2cfdd2c9d6af7
Javier <javier.arredondo.c@usach.cl>
2018-11-21 14:39:32 -0300

    Sin sala Isidora Aguirre

--- src/components/title/Title.vue
+++ src/components/title/Title.vue
@@ -5,7 +5,7 @@
         <div class="separator"> </div>
         <h3>  12 - 13 - 14 / Diciembre de 2018 </h3>
         <h4>  Edificio de Vicerrectoría de Vinculación con el Medio - Universidad de 
-            Santiago de Chile, Sala Isidora Aguirre. Las Sophoras 119, Estación Central, Santiago </h4>
+            Santiago de Chile. Las Sophoras 119, Estación Central, Santiago </h4>
         <a href="https://goo.gl/wTr13f">Afiche EVIC</a>
     </div>
 </template>
