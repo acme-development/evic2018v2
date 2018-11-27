97b6ffb1d4beef2bfb2e50f5521dd68c7ac3dffe
Javier <javier.arredondo.c@usach.cl>
2018-11-27 18:02:45 -0300

    adding afiche

--- src/components/title/Title.vue
+++ src/components/title/Title.vue
@@ -6,7 +6,7 @@
         <h3>  12 - 13 - 14 / Diciembre de 2018 </h3>
         <h4>  Edificio de Vicerrectoría de Vinculación con el Medio - Universidad de 
             Santiago de Chile. Las Sophoras 119, Estación Central, Santiago </h4>
-        <a href="https://goo.gl/wTr13f">Afiche EVIC</a>
+        <a href="https://drive.google.com/file/d/15zgccc5dfIOVKlQAg_Cavqi9Nv20caqZ/view?usp=sharing">Afiche EVIC</a>
     </div>
 </template>
 
