1cfb708d057ca073d94af63f41ca1f1fe9d6b6f6
Javier <javier.arredondo.c@usach.cl>
2018-11-29 12:57:03 -0300

    Cambio de dirreción y mapa

--- src/components/ubication/Ubication.vue
+++ src/components/ubication/Ubication.vue
@@ -11,7 +11,7 @@
 				<h2>{{ direccion }}</h2>
 			</div>
 			<div id = "map" class="container">
-				<iframe class ="map-content" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3329.122442362666!2d-70.68472348521755!3d-33.44611638077509!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662c4598d364523%3A0xd5ace8491dea67d6!2sCENI+-+Usach!5e0!3m2!1ses!2scl!4v1537459982272" width="100%" height="500" frameborder="0" style="border:0" allowfullscreen></iframe>
+				<iframe class ="map-content" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3329.1323634973664!2d-70.68575498480082!3d-33.44585788077532!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662c459879db2bf%3A0xc35d3910756cc624!2sEdificio+VIME+-+USACH!5e0!3m2!1ses-419!2scl!4v1543506684453" width="100%" height="500" frameborder="0" style="border:0" allowfullscreen></iframe>
 			</div>
 			<div class= "contenedor-exterior">
 				<div class ="contenedor-interior" v-for="(value, index) in firebaseInformacion" v-bind:key="index">
