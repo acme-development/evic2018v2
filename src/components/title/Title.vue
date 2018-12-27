63e2c5305010e2711e70bd743b662c88cc491cb5
michaelCastillo <michael.castillo.p96@gmail.com>
2018-12-27 17:09:11 -0300

    Se cambian las direcciones de las fotos

--- src/components/title/Title.vue
+++ src/components/title/Title.vue
@@ -34,35 +34,35 @@
 <script>
 var photoDir = '';
 var imageList = 
-  [{'name':'https://lh4.googleusercontent.com/k4FhsVHzFyEsOAODh3JgoDm74RyexJHKvdp8q3hA8lMUbp3iyK5YNHbuULvM_oohQOfKfnnlw_5qTDgsQ6UY=w1920-h951', 
+  [{'name':'https://github.com/acme-development/evic2018v2/blob/develop/src/assets/images/2.JPG?raw=true', 
     'alt':'Gary Fogel', 
     'filter':'evic' },
    
-   {'name':'https://lh3.googleusercontent.com/84jjXlB1ROb-l8cX8Ypqs763xm4wm1-RSeQpthPvHCysdVxijXegTyfNtpr0nBuG2W9S3qB29W76K27GhAjN=w1920-h951', 
+   {'name':'https://github.com/acme-development/evic2018v2/blob/develop/src/assets/images/5.JPG?raw=true', 
     'alt':'Pavlos Protopapas', 
     'filter':'evic' }, 
    
-   {'name':'https://lh6.googleusercontent.com/_NP8xch7zaHEKaYNfZm0tIpBnWoeA46ZxvbjUKJB6v0J4RMTo2ve6JwBPZFXbEjGX0KOH-F1_wFUm5wh0y8y=w1920-h951', 
+   {'name':'https://github.com/acme-development/evic2018v2/blob/develop/src/assets/images/4.JPG?raw=true', 
     'alt':'Millaray Curilem', 
     'filter':'nature' },
    
-   {'name':'https://lh5.googleusercontent.com/9db3u-5Hd2JQstns1A3kb5wJqlx86gamW6c5fUcgOcAwk6Ml9LQP-Ctpr08y5eGe3Tpgcavi5gRjUA3FOPIp=w1920-h951', 
+   {'name':'https://github.com/acme-development/evic2018v2/blob/develop/src/assets/images/7.JPG?raw=true', 
     'alt':'Gonzalo Acuña', 
     'filter':'evic' },
    
-   {'name':'https://lh6.googleusercontent.com/_Wd--xw7LM13flmJzjPojzKr-ZCYou36So1dL8u8In5v2dxbP1Qg-8XCYhtymQ5YpUmzdawdifxSBb5YSmDR=w1920-h951', 
+   {'name':'https://github.com/acme-development/evic2018v2/blob/develop/src/assets/images/8.JPG?raw=true', 
     'alt':'Participantes', 
     'filter':'nature' },
    
-   {'name':'https://lh3.googleusercontent.com/z1KYWrXruZI6Mui2wH-kdNEnNF5B3XO2bm-53XAKb5Ot0gAFGTmtoA26sGVzuvUPwenViWT6_3FuA9F5s-NP=w1920-h951', 
+   {'name':'https://github.com/acme-development/evic2018v2/blob/develop/src/assets/images/participantesMujeres.JPG?raw=true', 
     'alt':'Participantes mujeres', 
     'filter':'nature' },
    
-   {'name':'https://lh4.googleusercontent.com/plOcpSTTUftnY57ztoQffaQxcq4wCDeAca2KDjWlcLmEQakIKRDNTglSQDfilPj4PJzbvrR5F2yRt9VjPfEY=w1920-h951', 
+   {'name':'https://github.com/acme-development/evic2018v2/blob/develop/src/assets/images/6.JPG?raw=true', 
     'alt':'EVIC 2018', 
     'filter':'evic' }, 
    
-   {'name':'https://lh5.googleusercontent.com/mCXbrupEX0-uFO2QOiINQAfw7QnVQDzXKv2L9kmOeRetqsNPLsJ25LkGf3u6aaugPeC96JluzRnLudWKDPXi=w1920-h951', 
+   {'name':'https://raw.githubusercontent.com/acme-development/images/master/logo/logo.png', 
     'alt':'EVIC 2018', 
     'filter':'nature' } ];
 export default {
